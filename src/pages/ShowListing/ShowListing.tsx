c47e3e2c7db823d555b085794b424bfbc65f1ac1
branhickman <branhickman@gmail.com>
2023-02-24 11:34:33 -0700

    setup useEffect to fetchProfile on ShowListing component, setup getProfile function in profileService

--- src/pages/ShowListing/ShowListing.tsx
+++ src/pages/ShowListing/ShowListing.tsx
@@ -1,8 +1,29 @@
 import { useLocation } from "react-router";
+import { useState, useEffect } from "react";
+
+// types
+import { Profile } from "../../types/models";
+
+// services
+import * as profileService from '../../services/profileService'
 
 const ShowListing = () => {
   const location = useLocation()
   const listing = location.state
+
+  const [profile, setProfile] = useState<Profile>({})
+
+  useEffect((): void => {
+    const fetchProfile = async (): Promise<void> => {
+      try {
+        const profileData: Profile = await profileService.getProfile(listing.profileId)
+        setProfile(profileData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProfile()
+  })
 
   return (
     <>
