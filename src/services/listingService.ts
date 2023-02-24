976205ccd97bdfe2cd69aae21af9508c71812bd8
branhickman <branhickman@gmail.com>
2023-02-23 19:04:43 -0700

    stub up listingService

---
+++ src/services/listingService.ts
@@ -0,0 +1,16 @@
+// services
+import * as tokenService from './tokenService'
+
+// types
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/listings`
+
+// async function createListing(): {
+//   try {
+
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+export {  }
